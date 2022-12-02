bb4e099c2305989a912a193fe6e1e97f6c4eaa27
Marcus Vasconcelos <marcus.cunha@ntendencia.com.br>
2022-12-02 12:03:08 -0300

    Remoção de campo desnecessário

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,6 @@ import program.IASimulador;
 
 public class Main {
 
-    public String [] tabuleiro;
-
     public static void main(String[] args) {
         IASimulador iaSimulador = new IASimulador();
         iaSimulador.buscaAEstrela();
