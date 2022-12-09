71660ef106c20e30b88d10b63453285f4701845f
Marcus Vasconcelos <marcus.cunha@ntendencia.com.br>
2022-12-09 08:42:11 -0300

    Remoção de comentários e imports desnecessários

--- src/main/java/model/ArvoreSolucao.java
+++ src/main/java/model/ArvoreSolucao.java
@@ -1,18 +1,7 @@
-/*
- * Trabalho Prático: DCC146 - Aspectos Teóricos da Computação
- * Autores:
- * 
- * João Pedro Sequeto Nascimento - 201776022
- * Beatriz Cunha Rodrigues - 201776038
- * Marcus Vinícius V. A. Cunha - 201776013
- * Milles Joseph M e Silva - 201776026
- */
-
 package model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 import java.util.stream.Collectors;
 
 
